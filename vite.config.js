99b90768784242881670d1a6015ef2d24cf9794d
Shrouk-Saleh <shrouk26529@gmail.com>
2026-06-27 09:05:11 +0300

    ..

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1] || 'raven-ace-demo'
-
-export default defineConfig(({ command }) => ({
-  base: command === 'build' ? `/${repoName}/` : '/',
+export default defineConfig({
+  base: './',
   plugins: [react()],
   server: { port: 5173 }
-}))
+})
